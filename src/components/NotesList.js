e5c2691ccefdc6d4f4b865c70e05f0e2df5e1adf
ismaelmarot <ismaelmarot@hotmail.com>
2023-07-02 12:22:17 +1000

    Add React StyledComponents

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -4,6 +4,9 @@ const NoteList = () => {
     return(
         <div className='notes-list'>
             <Note />
+            <Note />
+            <Note />
+            <Note /> 
         </div>
     )
 }
